e9ff15ad0af1d5fc759ac40a0794b7e12371f907
liizzz <liizzz@foxmail.com>
2019-09-04 23:01:36 +0800

    generator

---
+++ src/main/java/com/lz/dao/SysAclMapper.java
@@ -0,0 +1,18 @@
+package com.lz.dao;
+
+import com.lz.model.SysAcl;
+
+public interface SysAclMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysAcl record);
+
+    int insertSelective(SysAcl record);
+
+    SysAcl selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SysAcl record);
+
+    int updateByPrimaryKey(SysAcl record);
+
+}
---
+++ src/main/java/com/lz/dao/SysRoleUserMapper.java
@@ -0,0 +1,17 @@
+package com.lz.dao;
+
+import com.lz.model.SysRoleUser;
+
+public interface SysRoleUserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysRoleUser record);
+
+    int insertSelective(SysRoleUser record);
+
+    SysRoleUser selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SysRoleUser record);
+
+    int updateByPrimaryKey(SysRoleUser record);
+}
